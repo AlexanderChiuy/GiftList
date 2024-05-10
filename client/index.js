38b7e7b4c89b5c97866b2d2a4c893a858f0012f0
Alexander Chiu <alexanderchiu@AlexandrsLaptop.lan>
2024-05-09 22:32:08 -0700

    prove and verify gift list recipient

--- client/index.js
+++ client/index.js
@@ -5,10 +5,14 @@ const MerkleTree = require('../utils/MerkleTree');
 const serverUrl = 'http://localhost:1225';
 
 async function main() {
-  // TODO: how do we prove to the server we're on the nice list? 
+  const tree = new MerkleTree(niceList);
 
+  const recipient = "Kim Mann";
+  const index = niceList.findIndex((i) => i === recipient);
+  const proof = tree.getProof(index || 0);
   const { data: gift } = await axios.post(`${serverUrl}/gift`, {
-    // TODO: add request body parameters here!
+    proof: proof,
+    recipient: recipient,
   });
 
   console.log({ gift });
